49531697a7a204872d19fc3f2eb2d6c0f5e378c7
jeffersonnunesfonseca <jeffersonnunesfonseca@gmail.com>
2022-07-06 08:33:44 -0300

    fix: example in config.py

--- importer_base_receita/config.py
+++ importer_base_receita/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:123456@localhost/basecnpj")
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:123456@localhost/base_cnpj")
